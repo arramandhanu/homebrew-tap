2e2314b6790b9df28a8c94382ea9948f8fc52396
Arya Ramandhanu <arya.ramandhanu@gmail.com>
2026-01-20 05:51:33 +0700

    feat: add Zsh completion support

--- Formula/shipctl.rb
+++ Formula/shipctl.rb
@@ -2,7 +2,7 @@ class Shipctl < Formula
   desc "Professional Docker deployment automation tool"
   homepage "https://github.com/arramandhanu/shipctl"
   url "https://github.com/arramandhanu/shipctl/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "fbdf8c75f9f12ebf75c8b866ee48cd2a6e4bb76023b6c8ac7856c66a6caf67c7"
+  sha256 "PLACEHOLDER_SHA256"
   license "MIT"
   version "1.0.0"
 
@@ -12,11 +12,20 @@ class Shipctl < Formula
   def install
     prefix.install Dir["*"]
     bin.install_symlink prefix/"shipctl" => "shipctl"
+    
+    # Install completions for both Bash and Zsh
     bash_completion.install prefix/"completions/shipctl.bash" => "shipctl"
+    zsh_completion.install prefix/"completions/shipctl.bash" => "_shipctl"
   end
 
   def caveats
     <<~EOS
+      Shell completions installed for Bash and Zsh.
+      
+      You may need to restart your shell or run:
+        source ~/.bashrc   # for Bash
+        source ~/.zshrc    # for Zsh
+      
       Run 'shipctl init' in your project directory to get started.
     EOS
   end
